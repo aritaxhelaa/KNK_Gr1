38b9ab37299310161247195e1c68b5c29575095e
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-04-25 09:39:11 +0200

    BaseRepository

---
+++ src/main/java/Database/DBConnector.java
@@ -0,0 +1,27 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private static Connection connection = null;
+
+    private static final String DB_URL = "jdbc:mysql://localhost/knk_2025";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection()  {
+        if(connection == null){
+            try{
+                connection = DriverManager.getConnection(DB_URL,USER,PASSWORD);}
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        return connection;
+
+    }
+
+}
