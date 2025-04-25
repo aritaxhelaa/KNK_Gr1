3c36051c57efaaee7a3386fef2472792e78da4ff
Arita <arita.xhela@student.uni-pr.edu>
2025-04-25 19:05:00 +0200

    Update

--- src/main/java/models/Dto/QytetiDto/UpdateQytetiDto.java
+++ src/main/java/models/Dto/QytetiDto/UpdateQytetiDto.java
@@ -1,4 +1,37 @@
 package models.Dto.QytetiDto;
 
 public class UpdateQytetiDto {
+    private int id;
+    private String emri;
+    private int komunaId;
+
+    public UpdateQytetiDto(int id, String emri, int komunaId) {
+        this.id = id;
+        this.emri = emri;
+        this.komunaId = komunaId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmri() {
+        return emri;
+    }
+
+    public void setEmri(String emri) {
+        this.emri = emri;
+    }
+
+    public int getKomunaId() {
+        return komunaId;
+    }
+
+    public void setKomunaId(int komunaId) {
+        this.komunaId = komunaId;
+    }
 }
