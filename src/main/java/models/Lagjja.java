df7367b9c7ec8d5543861267b9334c76991732a0
ErionMeh <erionm462@gmail.com>
2025-04-26 17:36:23 +0200

    Update Lagjja

--- src/main/java/models/Lagjja.java
+++ src/main/java/models/Lagjja.java
@@ -1,4 +1,25 @@
 package models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Lagjja {
+    private int id;
+    private String emri;
+    private int qytetiId;
+    private int komunaId;
+    private double siperfaqja;
+    private String pershkrimi;
+    private boolean statusiZyrtar;
+
+    private Lagjja(int id, String emri, int qytetiId, int komunaId, double siperfaqja, String pershkrimi, boolean statusiZyrtar) {
+        this.id = id;
+        this.emri = emri;
+        this.qytetiId = qytetiId;
+        this.komunaId = komunaId;
+        this.siperfaqja = siperfaqja;
+        this.pershkrimi = pershkrimi;
+        this.statusiZyrtar = statusiZyrtar;
+    }
+
 }
