ae319503b1fa736bcd2458bf2aad02d830903ef3
Arita <arita.xhela@student.uni-pr.edu>
2025-05-02 00:52:02 +0200

    added dto

--- src/main/java/models/Dto/TipiObjektitDto/UpdateTipiObjektitDto.java
+++ src/main/java/models/Dto/TipiObjektitDto/UpdateTipiObjektitDto.java
@@ -1,4 +1,27 @@
 package models.Dto.TipiObjektitDto;
 
 public class UpdateTipiObjektitDto {
+    private int id;
+    private String emri;
+
+    public UpdateTipiObjektitDto(int id, String emri) {
+        this.id = id;
+        this.emri = emri;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmri() {
+        return emri;
+    }
+
+    public void setEmri(String emri) {
+        this.emri = emri;
+    }
 }
