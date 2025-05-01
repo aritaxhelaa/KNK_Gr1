429b6a483579e219a1647a862ab99b5ecad22858
ErionMeh <erionm462@gmail.com>
2025-05-01 22:52:53 +0200

    Update Objekti

--- src/main/java/models/Objekti.java
+++ src/main/java/models/Objekti.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Objekti {
+    private int id;
+    private String lloji;
+    private String emri;
+    private int rrugaId;
+    private String numriNderteses;
+    private String koordinatatGPS;
+    private int kodiPostarId;
+
+    private Objekti(int id, String lloji, String emri, int rrugaId, String numriNderteses,
+                    String koordinatatGPS, int kodiPostarId) {
+        this.id = id;
+        this.lloji = lloji;
+        this.emri = emri;
+        this.rrugaId = rrugaId;
+        this.numriNderteses = numriNderteses;
+        this.koordinatatGPS = koordinatatGPS;
+        this.kodiPostarId = kodiPostarId;
+    }
 }
