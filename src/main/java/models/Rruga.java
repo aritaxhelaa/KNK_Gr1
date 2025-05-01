cd0d130ae41e5addeaaf825e45e5bd084e3132b2
ErionMeh <erionm462@gmail.com>
2025-05-02 00:12:18 +0200

    Rruga

--- src/main/java/models/Rruga.java
+++ src/main/java/models/Rruga.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Rruga {
+    private int id;
+    private String emri;
+    private int komunaId;
+    private int qytetiId;
+    private String kategoria;
+    private double gjatesiaKM;
+
+    private Rruga(int id, String emri, int komunaId, int qytetiId, String kategoria, double gjatesiaKM) {
+        this.id = id;
+        this.emri = emri;
+        this.komunaId = komunaId;
+        this.qytetiId = qytetiId;
+        this.kategoria = kategoria;
+        this.gjatesiaKM = gjatesiaKM;
+    }
 }
