92d14322d587e78fa06e00b3243721c99b3ca819
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-05-05 23:19:22 +0200

    UserDto

--- src/main/java/models/Dto/UserDto/UpdateUserDto.java
+++ src/main/java/models/Dto/UserDto/UpdateUserDto.java
@@ -1,4 +1,23 @@
 package models.Dto.UserDto;
 
 public class UpdateUserDto {
+    private int id;
+    private String email;
+
+    public UpdateUserDto(int id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
