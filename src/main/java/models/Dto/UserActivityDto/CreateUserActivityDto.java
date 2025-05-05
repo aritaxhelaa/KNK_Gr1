d23c7700af2272b6ab5df99740e58f32be722dff
Arita <arita.xhela@student.uni-pr.edu>
2025-05-05 17:29:22 +0200

    UserActivityDTO

--- src/main/java/models/Dto/UserActivityDto/CreateUserActivityDto.java
+++ src/main/java/models/Dto/UserActivityDto/CreateUserActivityDto.java
@@ -1,4 +1,47 @@
 package models.Dto.UserActivityDto;
 
 public class CreateUserActivityDto {
+    private int userId;
+    private String veprimi;
+    private String entiteti;
+    private int entitetiId;
+
+    public CreateUserActivityDto(int userId, String veprimi, String entiteti, int entitetiId) {
+        this.userId = userId;
+        this.veprimi = veprimi;
+        this.entiteti = entiteti;
+        this.entitetiId = entitetiId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getVeprimi() {
+        return veprimi;
+    }
+
+    public void setVeprimi(String veprimi) {
+        this.veprimi = veprimi;
+    }
+
+    public String getEntiteti() {
+        return entiteti;
+    }
+
+    public void setEntiteti(String entiteti) {
+        this.entiteti = entiteti;
+    }
+
+    public int getEntitetiId() {
+        return entitetiId;
+    }
+
+    public void setEntitetiId(int entitetiId) {
+        this.entitetiId = entitetiId;
+    }
 }
--- src/main/java/models/Dto/UserActivityDto/UpdateUserActivityDto.java
+++ src/main/java/models/Dto/UserActivityDto/UpdateUserActivityDto.java
@@ -1,4 +1,37 @@
 package models.Dto.UserActivityDto;
 
 public class UpdateUserActivityDto {
+    private String veprimi;
+    private String entiteti;
+    private int entitetiId;
+
+    public UpdateUserActivityDto(String veprimi, String entiteti, int entitetiId) {
+        this.veprimi = veprimi;
+        this.entiteti = entiteti;
+        this.entitetiId = entitetiId;
+    }
+
+    public String getVeprimi() {
+        return veprimi;
+    }
+
+    public void setVeprimi(String veprimi) {
+        this.veprimi = veprimi;
+    }
+
+    public String getEntiteti() {
+        return entiteti;
+    }
+
+    public void setEntiteti(String entiteti) {
+        this.entiteti = entiteti;
+    }
+
+    public int getEntitetiId() {
+        return entitetiId;
+    }
+
+    public void setEntitetiId(int entitetiId) {
+        this.entitetiId = entitetiId;
+    }
 }
