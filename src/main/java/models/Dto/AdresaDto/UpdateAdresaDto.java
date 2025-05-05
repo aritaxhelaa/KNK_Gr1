2539b1ca807f159f12dce69bb6ffd63aa6169e56
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-05-05 23:59:11 +0200

    AdresaRepository

--- src/main/java/models/Dto/AdresaDto/UpdateAdresaDto.java
+++ src/main/java/models/Dto/AdresaDto/UpdateAdresaDto.java
@@ -1,38 +1,43 @@
 package models.Dto.AdresaDto;
 
 public class UpdateAdresaDto {
+    private int id;
     private String rruga;
-    private int numri;
-    private int kodi_postar;
+    private Integer numri;
+    private Integer kodi_postar;
 
-    public UpdateAdresaDto(String rruga, int numri, int kodi_postar) {
+    public UpdateAdresaDto(int id, String rruga, Integer numri, Integer kodi_postar) {
+        this.id = id;
         this.rruga = rruga;
         this.numri = numri;
         this.kodi_postar = kodi_postar;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getRruga() {
         return rruga;
     }
 
-    public void setRruga(String rruga) {
-        this.rruga = rruga;
+    public Integer getNumri() {
+        return numri;
     }
 
-    public int getNumri() {
-        return numri;
+    public Integer getKodi_postar() {
+        return kodi_postar;
+    }
+
+    public void setRruga(String rruga) {
+        this.rruga = rruga;
     }
 
     public void setNumri(int numri) {
         this.numri = numri;
     }
 
-    public int getKodi_postar() {
-        return kodi_postar;
-    }
-
     public void setKodi_postar(int kodi_postar) {
         this.kodi_postar = kodi_postar;
     }
 }
-
--- src/main/java/repository/AdresaRepository.java
+++ src/main/java/repository/AdresaRepository.java
@@ -7,76 +7,79 @@ import models.Dto.AdresaDto.UpdateAdresaDto;
 import java.sql.*;
 import java.util.ArrayList;
 
+public class AdresaRepository extends BaseRepository<Adresa, CreateAdresaDto, UpdateAdresaDto> {
 
-
-public class AdresaRepository extends BaseRepository<Adresa, CreateAdresaDto, UpdateAdresaDto>{
-    public AdresaRepository(){
+    public AdresaRepository() {
         super("adresa");
     }
-    public Adresa fromResultSet(ResultSet result) throws SQLException{
+
+    @Override
+    public Adresa fromResultSet(ResultSet result) throws SQLException {
         return Adresa.getInstance(result);
     }
 
-    public Adresa create(CreateAdresaDto adresaDto){
-        String query ="""
-                INSERT INTO ADRESA(id, rruga, numri, kodiPostar)
-                 VALUES(?,?,?,?)
+    @Override
+    public Adresa create(CreateAdresaDto adresaDto) {
+        String query = """
+                INSERT INTO adresa (rruga, numri, kodiPostar)
+                VALUES (?, ?, ?)
                 """;
-        try{
-            PreparedStatement pstm=this.connection.prepareStatement(
-                    query,Statement.RETURN_GENERATED_KEYS);
-            pstm.setString(1,adresaDto.getRruga());
-            pstm.setInt(2,adresaDto.getNumri());
-            pstm.setInt(3,adresaDto.getKodi_postar());
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstm.setString(1, adresaDto.getRruga());
+            pstm.setInt(2, adresaDto.getNumri());
+            pstm.setInt(3, adresaDto.getKodi_postar());
             pstm.execute();
-            ResultSet resultSet=pstm.getGeneratedKeys();
-            if(resultSet.next()){
-                int id=resultSet.getInt(1);
+
+            ResultSet resultSet = pstm.getGeneratedKeys();
+            if (resultSet.next()) {
+                int id = resultSet.getInt(1);
                 return this.getById(id);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-//    public Adresa update(UpdateAdresaDto adresaDto){
-//        StringBuilder query=new StringBuilder("UPDATE ADRESA SET ");
-//        ArrayList<Object> params=new ArrayList<>();
-//
-//        if(adresaDto.getRruga() != null){
-//            query.append("RRUGA = ?, ");
-//            params.add(adresaDto.getRruga());
-//        }
-//        if(adresaDto.getNumri() != null){
-//            query.append("NUMRI = ?, ");
-//            params.add(adresaDto.getNumri());
-//        }
-//        if(adresaDto.getKodi_postar() != null){
-//            query.append("KODIPOSTAR = ?, ");
-//            params.add(adresaDto.getKodi_postar());
-//        }
-//        if(params.isEmpty()){
-//            return getById(adresaDto.getId());
-//        }
-//
-//        query.setLength(query.length() - 2);
-//        query.append(" WHERE ID = ?");
-//        params.add(adresaDto.getId());
-//
-//        try{
-//            PreparedStatement pstm=this.connection.prepareStatement(query.toString());
-//            for(int i = 0; i < params.size(); i++){
-//                pstm.setObject(i + 1, params.get(i));
-//            }
-//            int updated=pstm.executeUpdate();
-//            if(updated == 1) {
-//                return this.getById(adresaDto.getId());
-//            }
-//        }catch(SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//}
+    @Override
+    public Adresa update(UpdateAdresaDto adresaDto) {
+        StringBuilder query = new StringBuilder("UPDATE adresa SET ");
+        ArrayList<Object> params = new ArrayList<>();
 
+        if (adresaDto.getRruga() != null) {
+            query.append("rruga = ?, ");
+            params.add(adresaDto.getRruga());
+        }
+        if (adresaDto.getNumri() != null) {
+            query.append("numri = ?, ");
+            params.add(adresaDto.getNumri());
+        }
+        if (adresaDto.getKodi_postar() != null) {
+            query.append("kodiPostar = ?, ");
+            params.add(adresaDto.getKodi_postar());
+        }
+
+        if (params.isEmpty()) {
+            return getById(adresaDto.getId());
+        }
+
+        query.setLength(query.length() - 2);
+        query.append(" WHERE id = ?");
+        params.add(adresaDto.getId());
+
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(query.toString());
+            for (int i = 0; i < params.size(); i++) {
+                pstm.setObject(i + 1, params.get(i));
+            }
+            int updated = pstm.executeUpdate();
+            if (updated == 1) {
+                return this.getById(adresaDto.getId());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
