cf2914949c99f275983d75d420b64cac988de9ef
ErionMeh <erionm462@gmail.com>
2025-05-06 19:11:27 +0200

    database

--- src/main/java/Database/DBConnector.java
+++ src/main/java/Database/DBConnector.java
@@ -7,21 +7,20 @@ import java.sql.SQLException;
 public class DBConnector {
     private static Connection connection = null;
 
-    private static final String DB_URL = "jdbc:mysql://localhost/knk_2025";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/KNK_Gr1";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "0000";
 
-    public static Connection getConnection()  {
-        if(connection == null){
-            try{
-                connection = DriverManager.getConnection(DB_URL,USER,PASSWORD);}
-            catch (SQLException e){
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                System.out.println("Lidhja me databazën u realizua me sukses!");
+            } catch (SQLException e) {
+                System.out.println("Lidhja me databazën dështoi!");
                 e.printStackTrace();
             }
         }
-
         return connection;
-
     }
-
 }
---
+++ src/main/java/Database/TestConnection.java
@@ -0,0 +1,8 @@
+package Database;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        Database.DBConnector.getConnection();
+    }
+}
+
