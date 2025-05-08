aec2c3696214e69b72269ac3366a3047ff3efd09
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-05-09 01:25:10 +0200

    Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import services.SceneManager;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            SceneManager sceneManager = SceneManager.getInstance();
+            stage.setScene(sceneManager.getScene());
+            stage.setTitle("Sistemi për Regjistrim të Adresave");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
