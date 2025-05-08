37153d08e35b29c59fec2acac64dad1c8791f7c2
ErionMeh <erionm462@gmail.com>
2025-05-09 00:20:54 +0200

    Update LagjjaService.java

--- src/main/java/services/LagjjaService.java
+++ src/main/java/services/LagjjaService.java
@@ -1,4 +1,24 @@
 package services;
 
+
+import models.Lagjja;
+import repository.LagjjaRepository;
+
 public class LagjjaService {
+    private LagjjaRepository lagjjaRepository;
+
+    public LagjjaService(){
+        this.lagjjaRepository = new LagjjaRepository();
+    }
+
+    public Lagjja getById(int id) throws Exception{
+        if (id <= 0) {
+            throw new Exception("ID nuk është valid!");
+        }
+        Lagjja lagjja = this.lagjjaRepository.getById(id);
+        if (lagjja == null) {
+            throw new Exception("Lagjja me ID: " + id + " nuk ekziston.");
+        }
+        return lagjja;
+    }
 }
