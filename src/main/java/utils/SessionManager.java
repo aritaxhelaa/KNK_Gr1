4ba93a3f1d4151e90512b32484bf29ef58f93d09
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-05-08 15:03:26 +0200

    SessionManager

---
+++ src/main/java/utils/SessionManager.java
@@ -0,0 +1,23 @@
+package util;
+
+import models.User;
+
+public class SessionManager {
+    private static User currentUser;
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void clearSession() {
+        currentUser = null;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+}
