47209260d856e4720a0755515726509d42ae9b91
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-05-09 01:24:03 +0200

    SceneManager

--- src/main/java/services/SceneManager.java
+++ src/main/java/services/SceneManager.java
@@ -1,4 +1,52 @@
 package services;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import utils.SceneLocator;
+
 public class SceneManager {
+    private static SceneManager instance;
+    private Scene scene;
+    private String currentPath;
+
+    private SceneManager() {
+        this.currentPath = SceneLocator.LOGIN_PAGE;
+        this.scene = initScene();
+    }
+
+    public static SceneManager getInstance() {
+        if (instance == null) instance = new SceneManager();
+        return instance;
+    }
+
+    private Scene initScene() {
+        try {
+            return new Scene(loadFXML(currentPath));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void load(String path) throws Exception {
+        if (instance == null) throw new Exception("SceneManager not initialized!");
+        instance.setRoot(path);
+    }
+
+    private void setRoot(String path) throws Exception {
+        this.currentPath = path;
+        Parent root = loadFXML(path);
+        scene.setRoot(root);
+    }
+
+    private Parent loadFXML(String path) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+        return loader.load();
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
 }
--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -1,4 +1,8 @@
 package utils;
 
 public class SceneLocator {
+    public static final String LOGIN_PAGE = "/view/LoginView.fxml";
+//    public static final String ADMIN_DASHBOARD = "/views/admin-view.fxml";
+//    public static final String KOMUNAL_DASHBOARD = "/views/komunal-view.fxml";
+//    public static final String QYTETAR_DASHBOARD = "/views/qytetar-view.fxml";
 }
