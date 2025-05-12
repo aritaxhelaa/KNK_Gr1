b2cb96ff27d21c40bfedd4c783d44c54feaa0576
albanimani <alba.nimani@student.uni-pr.edu>
2025-05-12 14:14:59 +0200

    KomunaDto

--- src/main/java/models/Dto/KomunaDto/CreateKomunaDto.java
+++ src/main/java/models/Dto/KomunaDto/CreateKomunaDto.java
@@ -1,14 +1,13 @@
 package models.Dto.KomunaDto;
 
 public class CreateKomunaDto {
-//    private String emri;
-//
-//    public CreateKomunaDto(String emri) {
-//        this.emri = emri;
-//    }
-//
-//    public String getEmri() { return emri; }
-//    public void setEmri(String emri) { this.emri = emri; }
+    private String emri;
+    public CreateKomunaDto(String emri) {
+        this.emri = emri;
+    }
+
+   public String getEmri() { return emri; }
+   public void setEmri(String emri) { this.emri = emri; }
 }
 
 
